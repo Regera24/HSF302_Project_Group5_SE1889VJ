210ca60bf4a7ae33782fdfe4db5499bcb65a80d8
nebicod <dinhduchieu60@gmail.com>
2025-03-26 00:41:45 +0700

    update

--- coolcafe/src/main/java/org/group5/coolcafe/service/impl/OrderDetailServiceImpl.java
+++ coolcafe/src/main/java/org/group5/coolcafe/service/impl/OrderDetailServiceImpl.java
@@ -1,10 +1,24 @@
 package org.group5.coolcafe.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.group5.coolcafe.converter.OrderDetailConverter;
+import org.group5.coolcafe.dto.order.OrderDetail;
+import org.group5.coolcafe.repository.OrderDetailRepository;
 import org.group5.coolcafe.service.OrderDetailService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderDetailServiceImpl implements OrderDetailService {
+
+    private final OrderDetailRepository orderDetailRepository;
+    private final OrderDetailConverter orderDetailConverter;
+
+    @Override
+    public List<OrderDetail> getOrderDetailsByOrder(Long orderId) {
+        return orderDetailRepository.findByOrder_Id(orderId).stream().map(orderDetailConverter::toOrderDetail).collect(Collectors.toList());
+    }
 }
